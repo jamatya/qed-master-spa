1364c984aeffeb7053b6bc6f82209781602adfc1
Jeny <jenyamatya@gmail.com>
2022-12-07 11:56:46 +1000

    Added authentication via AAD B2C

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard, MsalRedirectComponent } from '@azure/msal-angular';
+import { BrowserUtils } from '@azure/msal-browser';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { GuardedComponent } from './guarded/guarded.component';
+
+const routes: Routes = [
+  {
+    path: 'guarded',
+    component: GuardedComponent,
+    canActivate: [
+        MsalGuard
+    ]
+  },
+  {
+      // Needed for handling redirect after login
+      path: 'auth',
+      component: MsalRedirectComponent
+  },
+  {
+      path: '',
+      component: HomeComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // Don't perform initial navigation in iframes or popups
+    initialNavigation: !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup() ? 'enabledNonBlocking' : 'disabled' // Set to enabledBlocking to use Angular Universal
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { IPublicClientApplication, PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import {
+    MsalGuard, MsalBroadcastService, MsalService,
+    MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalGuardConfiguration, MsalRedirectComponent, MsalModule
+} from '@azure/msal-angular';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +25,30 @@ import { FooterComponent } from './footer/footer.component';
 import { GhDocsComponent } from './gh-docs/gh-docs.component';
 import { BannerComponent } from './banner/banner.component';
 import { ApimComponent } from './apim/apim.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { loginRequest, msalConfig } from './auth-config';
+import { HomeComponent } from './home/home.component';
+import { GuardedComponent } from './guarded/guarded.component';
+
+/**
+ * Here we pass the configuration parameters to create an MSAL instance.
+ * For more info, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/configuration.md
+ */
+ export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication(msalConfig);
+}
+
+/**
+* Set your default interaction type for MSALGuard here. If you have any
+* additional scopes you want the user to consent upon login, add them here as well.
+*/
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+      interactionType: InteractionType.Redirect,
+      authRequest: loginRequest
+  };
+}
 
 @NgModule({
   declarations: [
@@ -16,13 +57,39 @@ import { ApimComponent } from './apim/apim.component';
     FooterComponent,
     GhDocsComponent,
     BannerComponent,
-    ApimComponent
+    ApimComponent,
+    NavbarComponent,
+    HomeComponent,
+    GuardedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule,
+    MatExpansionModule,
+    HttpClientModule,
+    FormsModule,
+    MsalModule    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+        provide: MSAL_GUARD_CONFIG,
+        useFactory: MSALGuardConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
